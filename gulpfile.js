dd943741fc8d8400f05ea90fddefc5c879fd59fe
ashokkumar.balasubramanian <email>
2021-10-01 15:42:13 +0530

    Update the showcase sample

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
-var webpackGulp = require('webpack-stream');
+var fs = require('fs');
+var gulp = require('gulp');
 var webpack = require('webpack');
+var webpackGulp = require('webpack-stream');
 
 /**
  * Bundle all module using webpack
@@ -18,14 +20,29 @@ gulp.task('build', ['ship-deps'], function(done) {
     if (!fs.existsSync('./styles')) {
         fs.mkdirSync('./styles');
     }
+    var runSequence = require('run-sequence');
     runSequence('bundle', done);
 });
 
 
 gulp.task('ship-deps', function(done) {
-    gulp.src(['./node_modules/@syncfusion/ej2/*.css', './node_modules/@syncfusion/ej2/dist/*{.js,.map}', './node_modules/fuse.js/dist/fuse.min.js','./node_modules/@syncfusion/ej2-build/src/utils/common.js'])
+    gulp.src(['./node_modules/@syncfusion/ej2/*.css', './node_modules/@syncfusion/ej2/dist/*{.js,.map}', './node_modules/fuse.js/dist/fuse.min.js'])
         .pipe(gulp.dest('./dist/'))
         .on('end', function() {
             done();
         });
+});
+/**
+ * Load the samples
+ */
+gulp.task('serve', ['build'], function (done) {
+    var browserSync = require('browser-sync');
+    var bs = browserSync.create('Essential JS 2');
+    var options = {
+        server: {
+            baseDir: './'
+        },
+        ui: false
+    };
+    bs.init(options, done);
 });
